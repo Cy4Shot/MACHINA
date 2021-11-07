517881bf0c79acf8ca34f7aebc8db28b82d8d9c4
Cy4Shot <veschi@gmail.com>
2021-11-07 22:07:33 +0000

    Empty World

---
+++ src/main/java/com/cy4/machina/world/DynamicStructureSettings.java
@@ -0,0 +1,12 @@
+package com.cy4.machina.world;
+
+import net.minecraft.world.gen.settings.DimensionStructuresSettings;
+
+public class DynamicStructureSettings extends DimensionStructuresSettings {
+
+	public DynamicStructureSettings() {
+		super(false);
+		this.structureConfig.clear();
+	}
+
+}
