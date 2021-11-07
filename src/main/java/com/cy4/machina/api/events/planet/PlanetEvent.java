babff6c4804a0c6b72c5be2c8e20fc636105feb5
matyrobbrt <robertmateimoraru@gmail.com>
2021-11-07 18:40:14 +0200

    datagen is fixed????

---
+++ src/main/java/com/cy4/machina/api/events/planet/PlanetEvent.java
@@ -0,0 +1,22 @@
+package com.cy4.machina.api.events.planet;
+
+import net.minecraft.world.World;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.Event;
+
+public abstract class PlanetEvent extends Event {
+
+	private final World planet;
+
+	protected PlanetEvent(World planet) {
+		this.planet = planet;
+	}
+
+	public World getPlanet() { return this.planet; }
+	
+	public void onPlanetCreated(World planet) {
+		MinecraftForge.EVENT_BUS.post(new OnPlanetCreatedEvent(planet));
+	}
+
+}
