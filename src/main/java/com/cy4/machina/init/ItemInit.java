371f601cbbfa747a20e6145db600e155972efded
matyrobbrt <robertmateimoraru@gmail.com>
2021-11-08 13:40:29 +0200

    ... 3rd time i put it back

--- src/main/java/com/cy4/machina/init/ItemInit.java
+++ src/main/java/com/cy4/machina/init/ItemInit.java
@@ -9,17 +9,10 @@ import net.minecraft.item.Item;
 
 @RegistryHolder
 public class ItemInit {
-	/**
+
 	@RegisterItem("item_group_icon")
 	public static final Item ITEM_GROUP_ICON = new Item(new Item.Properties().tab(MACHINA_ITEM_GROUP)) {
 		@Override
-		public void fillItemCategory(net.minecraft.item.ItemGroup pGroup,
-				net.minecraft.util.NonNullList<net.minecraft.item.ItemStack> pItems) {
-			// We need to hide the item
-		}
+		public void fillItemCategory(net.minecraft.item.ItemGroup pGroup, net.minecraft.util.NonNullList<net.minecraft.item.ItemStack> pItems) {};
 	};
-	**/
-
-	@RegisterItem("item_group_icon")
-	public static final Item ITEM_GROUP_ICON = new Item(new Item.Properties());
 }
