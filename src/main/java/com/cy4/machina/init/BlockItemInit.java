fe47f3424f0ff25d5fb371964e69d4cb75b3579f
FireTamer81 <46015982+FireTamer81@users.noreply.github.com>
2021-11-15 20:25:05 -0500

    Scratch that, now BlockItemInit is good
    For some reason the AUTO_BLOCK_ITEMS thing didn't do what it's name suggests. Kept it there but added back what I had as well.

--- src/main/java/com/cy4/machina/init/BlockItemInit.java
+++ src/main/java/com/cy4/machina/init/BlockItemInit.java
@@ -1,11 +1,44 @@
 package com.cy4.machina.init;
 
+import static com.cy4.machina.Machina.MACHINA_ITEM_GROUP;
+
+import com.cy4.machina.api.annotation.registries.RegisterBlockItem;
 import com.cy4.machina.api.annotation.registries.RegistryHolder;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 
 @RegistryHolder
 public final class BlockItemInit {
 
 	public static final Block[] AUTO_BLOCK_ITEMS = new Block[] {};
+	
+	@RegisterBlockItem
+	public static final BlockItem ROCKET_PLATFORM_BLOCKITEM = new BlockItem(BlockInit.ROCKET_PLATFORM_BLOCK,
+			new Item.Properties().tab(MACHINA_ITEM_GROUP));
+
+	@RegisterBlockItem
+	public static final BlockItem ANIMATED_BUILDER_MOUNT_BLOCKITEM = new BlockItem(BlockInit.ANIMATED_BUILDER_MOUNT,
+			new Item.Properties().tab(MACHINA_ITEM_GROUP));
+
+	@RegisterBlockItem
+	public static final BlockItem ANIMATED_BUILDER_BLOCKITEM = new BlockItem(BlockInit.ANIMATED_BUILDER,
+			new Item.Properties().tab(MACHINA_ITEM_GROUP));
+
+	@RegisterBlockItem
+	public static final BlockItem PAD_SIZE_RELAY_BLOCKITEM = new BlockItem(BlockInit.PAD_SIZE_RELAY,
+			new Item.Properties().tab(MACHINA_ITEM_GROUP));
+
+	@RegisterBlockItem
+	public static final BlockItem CONSOLE_BLOCKITEM = new BlockItem(BlockInit.CONSOLE,
+			new Item.Properties().tab(MACHINA_ITEM_GROUP));
+	
+	@RegisterBlockItem
+	public static final BlockItem ROCKET_BLOCKITEM = new BlockItem(BlockInit.ROCKET,
+			new Item.Properties().tab(MACHINA_ITEM_GROUP));
+	
+	@RegisterBlockItem
+	public static final BlockItem ROCKET_MOUNT_BLOCKITEM = new BlockItem(BlockInit.ROCKET_MOUNT,
+			new Item.Properties().tab(MACHINA_ITEM_GROUP));
 }
