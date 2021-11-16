7c826308d699d95bcef118b768b844dbc1d4ce85
FireTamer81 <46015982+FireTamer81@users.noreply.github.com>
2021-11-15 20:17:52 -0500

    Fixed BlockItemInit
    I had accidentally went with my class instead of the one that was already there in my last commit. Looked at commit history and changed it back

--- src/main/java/com/cy4/machina/init/BlockItemInit.java
+++ src/main/java/com/cy4/machina/init/BlockItemInit.java
@@ -1,41 +1,11 @@
 package com.cy4.machina.init;
 
-import static com.cy4.machina.Machina.MACHINA_ITEM_GROUP;
-
-import com.cy4.machina.api.annotation.registries.RegisterBlockItem;
 import com.cy4.machina.api.annotation.registries.RegistryHolder;
 
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
+import net.minecraft.block.Block;
 
 @RegistryHolder
-public class BlockItemInit {
+public final class BlockItemInit {
 
-	@RegisterBlockItem
-	public static final BlockItem ROCKET_PLATFORM_BLOCKITEM = new BlockItem(BlockInit.ROCKET_PLATFORM_BLOCK,
-			new Item.Properties().tab(MACHINA_ITEM_GROUP));
-
-	@RegisterBlockItem
-	public static final BlockItem ANIMATED_BUILDER_MOUNT_BLOCKITEM = new BlockItem(BlockInit.ANIMATED_BUILDER_MOUNT,
-			new Item.Properties().tab(MACHINA_ITEM_GROUP));
-
-	@RegisterBlockItem
-	public static final BlockItem ANIMATED_BUILDER_BLOCKITEM = new BlockItem(BlockInit.ANIMATED_BUILDER,
-			new Item.Properties().tab(MACHINA_ITEM_GROUP));
-
-	@RegisterBlockItem
-	public static final BlockItem PAD_SIZE_RELAY_BLOCKITEM = new BlockItem(BlockInit.PAD_SIZE_RELAY,
-			new Item.Properties().tab(MACHINA_ITEM_GROUP));
-
-	@RegisterBlockItem
-	public static final BlockItem CONSOLE_BLOCKITEM = new BlockItem(BlockInit.CONSOLE,
-			new Item.Properties().tab(MACHINA_ITEM_GROUP));
-
-	@RegisterBlockItem
-	public static final BlockItem ROCKET_BLOCKITEM = new BlockItem(BlockInit.ROCKET,
-			new Item.Properties().tab(MACHINA_ITEM_GROUP));
-	
-	@RegisterBlockItem
-	public static final BlockItem ROCKET_MOUNT_BLOCKITEM = new BlockItem(BlockInit.ROCKET_MOUNT,
-			new Item.Properties().tab(MACHINA_ITEM_GROUP));
+	public static final Block[] AUTO_BLOCK_ITEMS = new Block[] {};
 }
