632820cc919b4f8ce070486fd1ddfcea34707a76
matyrobbrt <robertmateimoraru@gmail.com>
2021-11-20 18:07:56 +0200

    Update ServerHelper.java

--- src/main/java/com/cy4/machina/api/util/helper/ServerHelper.java
+++ src/main/java/com/cy4/machina/api/util/helper/ServerHelper.java
@@ -3,22 +3,19 @@ package com.cy4.machina.api.util.helper;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
+import com.google.common.collect.Lists;
+
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.server.ServerWorld;
 
 public class ServerHelper {
-	
+
 	private ServerHelper() {
 	}
 
 	public static Optional<ServerWorld> getLevelFromServer(MinecraftServer server, ResourceLocation dimensionID) {
-		AtomicReference<ServerWorld> levelFound = new AtomicReference<>(null);
-		server.getAllLevels().forEach(level -> {
-			if (level.dimension().location().equals(dimensionID)) {
-				levelFound.set(level);
-			}
-		});
-		return Optional.ofNullable(levelFound.get());
+		return Lists.newArrayList(server.getAllLevels()).stream()
+				.filter(level -> level.dimension().location().equals(dimensionID)).findFirst();
 	}
 }
