aff6d14e1eade66ee97a1c3342e8af0ec51a3811
matyrobbrt <matyrobbrt@gmail.com>
2021-11-25 15:36:38 +0200

    fix

--- src/main/java/com/cy4/machina/block/PadSizeRelay.java
+++ src/main/java/com/cy4/machina/block/PadSizeRelay.java
@@ -1,3 +1,9 @@
+/**
+ * This code is part of the Machina Minecraft (Java Edition) mod and is licensed under the MIT license.
+ * If you want to contribute please join https://discord.com/invite/x9Mj63m4QG.
+ * More information can be found on Github: https://github.com/Cy4Shot/MACHINA
+ */
+
 package com.cy4.machina.block;
 
 import javax.swing.Timer;
