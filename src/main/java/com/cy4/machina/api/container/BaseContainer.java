507b3845b8a0eab18314e41f06a64a0a05238a45
matyrobbrt <matyrobbrt@gmail.com>
2021-11-25 15:32:43 +0200

    Revert "Added headers so that it seems like we are professionals."
    This reverts commit adb62886c2ce87513ef2e3f73cb4819bdcd97070.

--- src/main/java/com/cy4/machina/api/container/BaseContainer.java
+++ src/main/java/com/cy4/machina/api/container/BaseContainer.java
@@ -1,9 +1,3 @@
-/*
- * This code is part of the Machina Minecraft (Java Edition) mod and is licensed under the MIT license.
- * If you want to contribute please join https://discord.com/invite/x9Mj63m4QG.
- * More information can be found on Github: https://github.com/Cy4Shot/MACHINA
- */
-
 package com.cy4.machina.api.container;
 
 import net.minecraft.entity.player.PlayerEntity;
