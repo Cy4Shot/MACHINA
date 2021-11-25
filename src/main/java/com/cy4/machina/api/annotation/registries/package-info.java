adb62886c2ce87513ef2e3f73cb4819bdcd97070
matyrobbrt <matyrobbrt@gmail.com>
2021-11-25 15:13:26 +0200

    Added headers so that it seems like we are professionals.
    Signed-off-by: matyrobbrt <matyrobbrt@gmail.com>

--- src/main/java/com/cy4/machina/api/annotation/registries/package-info.java
+++ src/main/java/com/cy4/machina/api/annotation/registries/package-info.java
@@ -1,3 +1,9 @@
+/*
+ * This code is part of the Machina Minecraft (Java Edition) mod and is licensed under the MIT license.
+ * If you want to contribute please join https://discord.com/invite/x9Mj63m4QG.
+ * More information can be found on Github: https://github.com/Cy4Shot/MACHINA
+ */
+
 /**
  * This package holds (and handles) all of the Registry Annotations
  *
