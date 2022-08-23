59e57700ea1f7fbc1e50c15293325745070c5ac5
Cy4 <veschi@gmail.com>
2022-08-23 19:36:32 +0100

    Render Other Side of Custom Models

--- src/main/java/com/machina/client/renderer/CustomModelRenderer.java
+++ src/main/java/com/machina/client/renderer/CustomModelRenderer.java
@@ -6,10 +6,13 @@ import com.machina.block.tile.CustomModelTileEntity;
 import com.machina.client.model.CustomBlockModel;
 import com.machina.util.reflection.ClassHelper;
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import software.bernie.geckolib3.renderers.geo.GeoBlockRenderer;
 
@@ -32,5 +35,12 @@ public class CustomModelRenderer extends GeoBlockRenderer<CustomModelTileEntity>
 			e.printStackTrace();
 		}
 		super.render(te, partialTicks, stack, bufferIn, combinedLightIn, combinedOverlayIn);
+	}
+	
+	@Override
+	public RenderType getRenderType(CustomModelTileEntity animatable, float partialTicks, MatrixStack stack,
+			IRenderTypeBuffer renderTypeBuffer, IVertexBuilder vertexBuilder, int packedLightIn,
+			ResourceLocation textureLocation) {
+		return RenderType.entityTranslucent(textureLocation);
 	}
 }
