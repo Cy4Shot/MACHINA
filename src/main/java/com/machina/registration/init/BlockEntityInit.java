dc4b327dc0e34c2cd707a7bdc9e8ae421f1b6ba1
Cy4 <veschi@gmail.com>
2024-01-26 11:28:22 +0000

    Remove old test data

--- src/main/java/com/machina/registration/init/BlockEntityInit.java
+++ src/main/java/com/machina/registration/init/BlockEntityInit.java
@@ -3,9 +3,6 @@ package com.machina.registration.init;
 import java.util.function.Supplier;
 
 import com.machina.Machina;
-import com.machina.block.tile.machine.TestBE;
-import com.machina.block.tile.multiblock.TestMasterMBE;
-import com.machina.block.tile.multiblock.TestPartMBE;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -17,13 +14,6 @@ import net.minecraftforge.registries.RegistryObject;
 public class BlockEntityInit {
 	public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_TYPES = DeferredRegister
 			.create(ForgeRegistries.BLOCK_ENTITY_TYPES, Machina.MOD_ID);
-
-	public static final RegistryObject<BlockEntityType<TestBE>> TEST = register("test", TestBE::new,
-			() -> BlockInit.TEST.get());
-	public static final RegistryObject<BlockEntityType<TestMasterMBE>> TEST_MASTER = register("test_master",
-			TestMasterMBE::new, () -> BlockInit.TEST_MASTER.get());
-	public static final RegistryObject<BlockEntityType<TestPartMBE>> TEST_PART = register("test_part", TestPartMBE::new,
-			() -> BlockInit.TEST_PART.get());
 
 	private static <T extends BlockEntity> RegistryObject<BlockEntityType<T>> register(String n,
 			BlockEntityType.BlockEntitySupplier<T> s, Supplier<Block> b) {
--- src/main/java/com/machina/registration/init/MultiblockInit.java
+++ src/main/java/com/machina/registration/init/MultiblockInit.java
@@ -1,12 +1,8 @@
 package com.machina.registration.init;
 
-import com.machina.api.util.MachinaRL;
-
-import net.minecraft.resources.ResourceLocation;
-
 // However elementary, this class is probably useful for consistency's sake.
 public class MultiblockInit {
 	
-	public static final ResourceLocation TEST = new MachinaRL("test");
+//	public static final ResourceLocation TEST = new MachinaRL("test");
 
 }
